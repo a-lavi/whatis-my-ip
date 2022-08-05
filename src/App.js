1740c30a1e1472beac3c408b85f13c172474b9fd
a-lavi <ali.rahmani.63@icloud.com>
2022-08-05 10:45:19 +0200

    add git

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import { useState, useEffect } from "react";
 function App() {
+  const url= `https://geo.ipify.org/api/v2/country?apiKey=at_OG5OaGTGRirg0y6bZpWuS9Y2VgAH9&ipAddress`
+  const [queryString, setQueryString] = useState(url)
+  const [ipInfo,setIpInfo] = useState({})
+  const [error, setError] = useState("")
+  const [loading, setLoading] = useState(false)
+  useEffect(() => { getData() }, [queryString])
+  const getData = async () => {
+    setLoading(true)
+    const response = await fetch(queryString)
+    let result = ""
+    console.log(response)
+  try{
+    if (response.ok) {
+      result = await response.json();
+      }
+      if (result.length === 0) {
+        alert("!!!")
+      }
+      console.log(result)
+      setIpInfo(() => result)
+      
+
+    } catch (error) {
+      console.error("2!!!")
+      setError("3!!!");}}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>{ipInfo.ip}</h1>
     </div>
   );
 }
