cc7cadd3c27a698c3ccdb368046f740f5c5bbc48
a-lavi <ali.rahmani.63@icloud.com>
2022-08-06 17:14:30 +0200

    add time

--- src/Components/ShowMap.js
+++ src/Components/ShowMap.js
@@ -1,7 +1,11 @@
 import React ,{useState}from 'react'
 import { Map, Marker } from "pigeon-maps"
 import Countries from './Countries';
+import Button from '@mui/material/Button';
+import { DateTime } from "luxon";
 const ShowMap = ({ipInfo}) => {
+  const now = DateTime.now();
+  console.log(now)
   const [showInfo, setShowInfo] = useState(false);
   const clickHandler = ()=> {
     setShowInfo (!showInfo)
@@ -25,8 +29,9 @@ IPv4: {ipInfo.ip}
   height="120"
   alt="South Africa"></img>
   <div className="searchflex">
-          
-          <button to="/search" className="searchButton" onClick={clickHandler}>More Info</button>
+          <div>Date: {`${now.year}/${now.month}/${now.day}`}</div>
+          <div>Time: {`${now.hour}:${now.minute}`}</div>
+          <Button variant="contained" to="/search" className="searchButton" onClick={clickHandler}>More Info</Button>
           </div>
           <div>
             <Countries show={showInfo}country={ipInfo.location.country}/>
